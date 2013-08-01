1e6277c06a1add0e0daa78057a88903292711a09
Ming Liu <nongnongd@yahoo.com>
2013-07-31 20:20:00 -0400

    hhj

--- network/src/main/java/network/multicast/Subscriber.java
+++ network/src/main/java/network/multicast/Subscriber.java
@@ -4,19 +4,41 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.Arrays;
 
 public class Subscriber {
-	public void subscribe(String group, int port, MessageListener listener) throws IOException {
-		MulticastSocket socket = new MulticastSocket(port);
+	private MulticastSocket socket;
+	public void subscribe(final String group, final int port, final MessageListener listener) throws IOException {
+		socket = new MulticastSocket(port);
 		socket.joinGroup(InetAddress.getByName(group));
 		
-		byte[] buffer = new byte[1024];
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		Thread messageListeningThread = new Thread(new Runnable(){
+
+			public void run() {
+				byte[] buffer = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				while(true) {
+					try {
+						socket.receive(packet);
+						byte[] data = Arrays.copyOfRange(packet.getData(), packet.getOffset(), packet.getOffset() + packet.getLength());
+						packet.getOffset();
+						packet.getLength();
+						listener.messageReceived(group, port, data);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+			
+		}, "MessageListeningThread:" + group + ":" + port);
+		messageListeningThread.start();
 		
-		while(true) {
-			socket.receive(packet);
-			System.out.println("gotit");
-		}
-		
+
+	}
+	
+	@Override
+	public void finalize() {
+		socket.close();
 	}
 }
